818cc439bfd24948c914d1d045a3e3331dd46c74
suqun zhang <susizhang2017@gmail.com>
2022-10-25 21:14:09 +0200

    Adding footer and other files

--- hackernews/src/components/Article.js
+++ hackernews/src/components/Article.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-const Article = () => {
-  return <div></div>;
+const Article = ({ title, url, points, author, created_at, num_comments }) => {
+  return (
+    <div className="search-result-container">
+      <article className="search-article">
+        <div className="article-top">
+          <span>{title}</span>
+          <span>{url}</span>
+        </div>
+        <div className="article-bottom">
+          <span>
+            <span href=""> {points} points </span>
+            <span>|</span>
+          </span>
+          <span>
+            <span href=""> {author}</span>
+            <span>|</span>
+          </span>
+          <span>
+            <span href=""> {created_at} years ago</span>
+            <span>|</span>
+          </span>
+          <span>
+            <span href=""> {num_comments} comments</span>
+            <span>|</span>
+          </span>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default Article;
--- hackernews/src/components/Main.js
+++ hackernews/src/components/Main.js
@@ -4,9 +4,18 @@ import Article from "./Article";
 
 const Main = () => {
   return (
-    <div>
+    <div className="main-container">
       <Filter />
       <Article />
+      <div className="main-page-button">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button> >> </button>
+      </div>
     </div>
   );
 };
