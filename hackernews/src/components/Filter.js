646eb23bda036df316d8fd9928234034b502f74a
suqun zhang <susizhang2017@gmail.com>
2022-10-25 20:07:38 +0200

    Adding filter section

--- hackernews/src/components/Filter.js
+++ hackernews/src/components/Filter.js
@@ -1,7 +1,42 @@
 import React from "react";
 
-const Filter = () => {
-  return <div></div>;
+const filter = () => {
+  return (
+    <div className="filter-container">
+      <div className="filter-option">
+        <span>Search</span>
+        <select className="select-options">
+          <option selected value="All">
+            All
+          </option>
+          <option value="Stories">Stories</option>
+          <option value="Comments">Comments</option>
+        </select>
+      </div>
+      <div className="filter-option">
+        <span>by</span>
+        <select className="select-options">
+          <option selected value="Popularity">
+            Popularity
+          </option>
+          <option value="Date">Date</option>
+        </select>
+      </div>
+      <div className="filter-option">
+        <span>for</span>
+        <select className="select-options">
+          <option selected value="All time">
+            All time
+          </option>
+          <option value="Last 24h">Last 24h</option>
+          <option value="Past Week">Past Week</option>
+          <option value="Past Month">Past Month</option>
+          <option value="Past Year">Past Year</option>
+          <option value="Custom range">Custom range</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
-export default Filter;
+export default filter;
