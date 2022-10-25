4f5ef948739c468ae3fb77b9cde45c957fd90b9a
Vinay Rao <vrao8912@gmail.com>
2022-10-25 17:38:35 +0200

    added hackerdata to src

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import hackerdata from "./hackerdata.json";
 
 function App() {
+  console.log(hackerdata)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header class="SearchHeader container">
+            <div class="SearchHeader_search">
+              
+            </div>
+        </header>
     </div>
   );
 }
---
+++ hackernews/src/Searchbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Searchbar() {
+  return (
+    <div>
+        <header class="SearchHeader container">
+            <div class="SearchHeader_search">
+            <span class="SearchHeader_logo">
+                
+                    <a href="/"><div class="SearchHeader_label">Search<br>Hacker News</div></a></span>
+            </div>
+        </header>
+    </div>
+  )
+};
