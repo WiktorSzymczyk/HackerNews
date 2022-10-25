8486a6bc79d8240884947fafe3da4f48a4d7fd30
suqun zhang <susizhang2017@gmail.com>
2022-10-25 18:09:46 +0200

    Adding logo image

--- hackernews/src/components/Header.js
+++ hackernews/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Search from "./Search";
+import Logo from "../asset/logo-hn-search-a822432b.webp";
 
 const Header = () => {
   return (
     <div>
+      <img src={Logo} alt="Logo" />
+      <h1 className="header-title">
+        Search <br /> Hacker News
+      </h1>
+
       <Search />
     </div>
   );
